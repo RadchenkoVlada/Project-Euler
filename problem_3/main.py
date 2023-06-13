524f904680f7ecc3c9950d06248d5d3745312af1
Vlada Radchenko <vlada_radchenko@epam.com>
2023-06-13 17:51:15 +0300

    The skeleton of program was created.

---
+++ problem_3/main.py
@@ -0,0 +1,14 @@
+def the_largest_prime_factor() -> int:
+    """
+    The prime factors of 13195 are 5, 7, 13 and 29.
+    What is the largest prime factor of the number 600851475143?
+
+    expects: the number
+    modifies: nothing
+    returns: the largest prime factor of the number
+    """
+
+
+
+if __name__ == '__main__':
+    print(the_largest_prime_factor())
